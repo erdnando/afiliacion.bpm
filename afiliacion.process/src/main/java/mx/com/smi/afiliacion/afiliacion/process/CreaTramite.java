2b12ffbea61858638330f7168aceea3a16fbf3eb
erdnando <erdnando@ubuntu01>
2019-08-22 12:12:56 -0500

    ok

--- afiliacion.process/src/main/java/mx/com/smi/afiliacion/afiliacion/process/CreaTramite.java
+++ afiliacion.process/src/main/java/mx/com/smi/afiliacion/afiliacion/process/CreaTramite.java
@@ -12,7 +12,7 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import mx.com.smi.afiliacion.services.*;  
 
 
-//crea tramite en bpm ok
+//crea tramite en bpm ok ok
 public class CreaTramite implements JavaDelegate {
 
 	@Override
